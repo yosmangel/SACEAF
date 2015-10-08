16a43c0eb5699bcc31ffccd0fcdbf78015d2188d
yosmangel <yadames.sybven@gmail.com>
2015-10-07 22:52:13 -0430

    limpieza del modulo de cronograma

--- SACEAF/src/torneocronograma/verjornada.java
+++ SACEAF/src/torneocronograma/verjornada.java
@@ -28,10 +28,13 @@ public class verjornada extends javax.swing.JPanel {
     static DefaultTableModel modelo= new DefaultTableModel();
     static DefaultComboBoxModel combo= new DefaultComboBoxModel();
     static int datogrupos;
-    String id_torneo, id_competencia,grupo,id_juego, id_categoria,datopostfase,nombre_modalidad,sexo,clublocal,clubvisitante,hlocal,hvisitante,idequipolocal,idequipovisitante;
-    String idequipob,idbuscar,idtorneo,categoria,jornada,letrag,id_club,idcategoria,nombre_local,identificador_local,nombre_visitante,identificador_visitante;
+    String id_torneo, id_competencia,grupo,id_juego, id_categoria,datopostfase,nombre_modalidad,sexo,clublocal,
+           clubvisitante,hlocal,hvisitante,idequipolocal,idequipovisitante, idequipob,idbuscar,idtorneo,categoria,
+            jornada,letrag,id_club,idcategoria,nombre_local,identificador_local,nombre_visitante,
+            identificador_visitante;
     int idnumero,numeroeq,ctrl,ronda;
     static String ncategoria, lsexo,serialtorneo,cidcategoria,letragrupo,id_modalidad; 
+   
     public verjornada() {
         initComponents();
         cargarcombo();
@@ -55,10 +58,8 @@ public class verjornada extends javax.swing.JPanel {
             cidcategoria=rs.getNString("id_categoria");
             break;
             }            
-            }catch(SQLException ex){
-            Logger.getLogger(dosgrupos.class.getName()).log(Level.SEVERE, null, ex);
-            }catch(ClassNotFoundException e){
-            Logger.getLogger(dosgrupos.class.getName()).log(Level.SEVERE,null,e);
+            }catch(SQLException | ClassNotFoundException ex){
+            Logger.getLogger(verjornada.class.getName()).log(Level.SEVERE, null, ex);
             }
      try{
             String[] titulos={"Fecha juego","Jor.","Nombre cancha","Hora","Equipo local"," ","Equipo visitante"," "};
@@ -113,7 +114,7 @@ public class verjornada extends javax.swing.JPanel {
           break;
           }
           }catch(SQLException | ClassNotFoundException ex){
-          Logger.getLogger(crearcopa.class.getName()).log(Level.SEVERE,null,ex);
+          Logger.getLogger(verjornada.class.getName()).log(Level.SEVERE,null,ex);
           }
           try{
           Conexion parametros= new Conexion();
@@ -126,7 +127,7 @@ public class verjornada extends javax.swing.JPanel {
           break;
           }
           }catch(SQLException | ClassNotFoundException ex){
-          Logger.getLogger(crearcopa.class.getName()).log(Level.SEVERE,null,ex);
+          Logger.getLogger(verjornada.class.getName()).log(Level.SEVERE,null,ex);
           }
           try{
           Conexion parametros= new Conexion();
@@ -139,7 +140,7 @@ public class verjornada extends javax.swing.JPanel {
           break;
           }
           }catch(SQLException | ClassNotFoundException ex){
-          Logger.getLogger(crearcopa.class.getName()).log(Level.SEVERE,null,ex);
+          Logger.getLogger(verjornada.class.getName()).log(Level.SEVERE,null,ex);
           }
           if(datogrupos!=0){
           combogrupo.removeAllItems();
@@ -155,7 +156,7 @@ public class verjornada extends javax.swing.JPanel {
           }
           st.close();
           }catch(SQLException | ClassNotFoundException ex){
-          Logger.getLogger(Semifinal.class.getName()).log(Level.SEVERE,null,ex);
+          Logger.getLogger(verjornada.class.getName()).log(Level.SEVERE,null,ex);
           }
           }
   }
