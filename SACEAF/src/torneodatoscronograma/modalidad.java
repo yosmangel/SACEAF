4ddcec25dab9868f263fbbb1e36146ed8b4a7749
yosmangel <yadames.sybven@gmail.com>
2015-10-17 22:30:51 -0430

    Ultimas modificaciones

--- SACEAF/src/torneodatoscronograma/modalidad.java
+++ SACEAF/src/torneodatoscronograma/modalidad.java
@@ -15,8 +15,9 @@ public class modalidad {
         Class.forName(parametros.getDriver());
         Connection con= DriverManager.getConnection(parametros.getURL(),parametros.getUsuario(), parametros.getPass());
         Statement st=con.createStatement();
-        String sql="Insert into modalidad values('"+user.getId_modalidad()+"','"+user.getNombre_modalidad()+"','"+user.getId_categoria()+"',"
-                + " '"+user.getDatopostfase()+"', '"+user.getId_torneo()+"');";
+        String sql="Insert into modalidad values('"+user.getId_modalidad()+"','"+user.getNombre_modalidad()+"',"
+                + "'"+user.getId_categoria()+"',"
+                + " '"+user.getDatopostfase()+"','"+user.getId_torneo()+"');";
         boolean result;
         result=st.execute(sql);
         st.close();
