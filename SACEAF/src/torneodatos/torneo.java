21f868b578b9900d25ff915c5dddbdb4f43973d4
yosmangel <yadames.sybven@gmail.com>
2015-11-04 16:25:19 -0430

    se agrego la sentencia de modificar el torneo

--- SACEAF/src/torneodatos/torneo.java
+++ SACEAF/src/torneodatos/torneo.java
@@ -24,4 +24,20 @@ public class torneo {
         return result;
         
     }
+    
+    public boolean Modificar(datostorneo user) throws SQLException, ClassNotFoundException {
+        Conexion parametros= new Conexion();
+        Class.forName(parametros.getDriver());
+        Connection con=DriverManager.getConnection(parametros.getURL(), parametros.getUsuario(), parametros.getPass());
+        Statement st=con.createStatement();
+        boolean result;
+        String sql="update torneo set nombre_t='"+user.getNombretorneo()+"', fecha_init='"+user.getFechaini()+"',"
+                + "fecha_fint='"+user.getFechafin()+"', id_disciplina='"+user.getDisciplina()+"',"
+                + "sexo='"+user.getSexo()+"' where id_torneo='"+user.getSerialtorneo()+"'";
+        result=st.execute(sql);
+        st.close();
+        con.close();
+        return result;
+        
+    }
 }
