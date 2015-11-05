6c9714d785bc50da9f2eb77009ad28c6a1bd0bca
yosmangel <yadames.sybven@gmail.com>
2015-11-04 23:53:54 -0430

    panel visible de modificar

--- SACEAF/src/torneocronograma/modificarcalendario.java
+++ SACEAF/src/torneocronograma/modificarcalendario.java
@@ -10,7 +10,9 @@ import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JOptionPane;
 import saceaf.Principal;
+import static torneocronograma.Gcronograma.panelcronograma;
 
 
 
@@ -125,6 +127,7 @@ public final class modificarcalendario extends javax.swing.JPanel {
         rasignarcancha = new javax.swing.JRadioButton();
         panelmodificar = new javax.swing.JTabbedPane();
         labelidjuego = new javax.swing.JLabel();
+        btnSalir = new javax.swing.JButton();
 
         setMinimumSize(new java.awt.Dimension(880, 330));
         setOpaque(false);
@@ -157,6 +160,20 @@ public final class modificarcalendario extends javax.swing.JPanel {
         panelmodificar.setMinimumSize(new java.awt.Dimension(860, 273));
         panelmodificar.setPreferredSize(new java.awt.Dimension(860, 273));
 
+        btnSalir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/iconossalir/exit6 30x30.png"))); // NOI18N
+        btnSalir.setText("Salir");
+        btnSalir.setContentAreaFilled(false);
+        btnSalir.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btnSalir.setIconTextGap(-2);
+        btnSalir.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/iconossalir/exit6 40x40.png"))); // NOI18N
+        btnSalir.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
+        btnSalir.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        btnSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSalirActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -164,9 +181,7 @@ public final class modificarcalendario extends javax.swing.JPanel {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(panelmodificar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addContainerGap())
+                    .addComponent(panelmodificar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(combocategoria, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
@@ -175,20 +190,25 @@ public final class modificarcalendario extends javax.swing.JPanel {
                         .addComponent(rasignarcancha, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(labelidjuego, javax.swing.GroupLayout.PREFERRED_SIZE, 12, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(88, 88, 88))))
+                        .addGap(15, 15, 15)
+                        .addComponent(btnSalir)))
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(combocategoria, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(rfechajornada)
-                        .addComponent(rasignarcancha))
-                    .addComponent(labelidjuego, javax.swing.GroupLayout.PREFERRED_SIZE, 11, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(combocategoria, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(rfechajornada)
+                                .addComponent(rasignarcancha))
+                            .addComponent(labelidjuego, javax.swing.GroupLayout.PREFERRED_SIZE, 11, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(btnSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(panelmodificar, javax.swing.GroupLayout.DEFAULT_SIZE, 284, Short.MAX_VALUE))
+                .addComponent(panelmodificar, javax.swing.GroupLayout.DEFAULT_SIZE, 278, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -204,11 +224,27 @@ public final class modificarcalendario extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void rfechajornadaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rfechajornadaActionPerformed
-       rasignarcancha.setSelected(false);
+       
+        try{
+        rasignarcancha.setSelected(false);
        
         if(rfechajornada.isSelected()==true){
+         try{
+         int tamano=panelmodificar.getTabCount();              
+         for(int i=0;i<=tamano;i++){
+                 panelmodificar.remove(i);
+                 i--;
+                 tamano=panelmodificar.getTabCount(); 
+                 if( tamano== -1){
+                 break;
+                 }
+                 }
+         
+       }catch(Exception e){
+           
+       } 
          bloquearcombo();
-         abrirfechajornada(); 
+         abrirfechajornada();
         }else
         if(rfechajornada.isSelected()==false){
             
@@ -228,12 +264,35 @@ public final class modificarcalendario extends javax.swing.JPanel {
            
        } 
         }
+        }catch(Exception em){
+            JOptionPane.showMessageDialog(null,"No posees juegos para asignar fecha","Información", JOptionPane.INFORMATION_MESSAGE);
+            rfechajornada.setSelected(false);
+            modificarcalendario.combocategoria.setEnabled(true);
+        }
     }//GEN-LAST:event_rfechajornadaActionPerformed
 
     private void rasignarcanchaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rasignarcanchaActionPerformed
-        rfechajornada.setSelected(false);
+        modificarcalendario.panelmodificar.remove(this);
+        try{
+        
+            rfechajornada.setSelected(false);
          if(rasignarcancha.isSelected()==true){
-         bloquearcombo();
+        
+         try{
+         int tamano=panelmodificar.getTabCount();              
+         for(int i=0;i<=tamano;i++){
+                 panelmodificar.remove(i);
+                 i--;
+                 tamano=panelmodificar.getTabCount(); 
+                 if( tamano== -1){
+                 break;
+                 }
+                 }
+        
+       }catch(Exception e){
+           
+       }     
+          bloquearcombo();
          abrirasignarcancha(); 
         }else
         if(rasignarcancha.isSelected()==false){
@@ -253,7 +312,11 @@ public final class modificarcalendario extends javax.swing.JPanel {
            
        } 
         }
-       
+       }catch(Exception em){
+            JOptionPane.showMessageDialog(null,"No posees juegos para asignar cancha","Información", JOptionPane.INFORMATION_MESSAGE);
+            rasignarcancha.setSelected(false);
+            modificarcalendario.combocategoria.setEnabled(true);
+        }
     }//GEN-LAST:event_rasignarcanchaActionPerformed
 
     private void combocategoriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_combocategoriaActionPerformed
@@ -262,8 +325,13 @@ public final class modificarcalendario extends javax.swing.JPanel {
 
     }//GEN-LAST:event_combocategoriaActionPerformed
 
+    private void btnSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalirActionPerformed
+        panelcronograma.remove(this);
+    }//GEN-LAST:event_btnSalirActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    public static javax.swing.JButton btnSalir;
     public static javax.swing.JComboBox combocategoria;
     public static javax.swing.JPanel jPanel1;
     public static javax.swing.JLabel labelidjuego;
